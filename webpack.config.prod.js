3b7fe4bd75dfbead717957869e85a779a0729bd7
Jack Franklin <jack@jackfranklin.net>
2016-07-21 16:59:38 +0200

    Push sourcemaps to production

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -5,6 +5,7 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  devtool: 'source-map',
   entry: {
     vendor: ['react', 'react-dom'],
     app: './src/index.js',
