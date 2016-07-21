ee7349b2dae7cb93fa46f9cee623c41c494b1804
Jack Franklin <jack@jackfranklin.net>
2016-07-21 16:46:01 +0200

    Configure the production build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,13 +1,18 @@
 var path = require('path');
 var webpack = require('webpack');
+var combineLoaders = require('webpack-combine-loaders');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  devtool: 'source-map',
   entry: './src/index.js',
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'javascripts-[hash].js'
   },
   plugins: [
+    new ExtractTextPlugin('styles-[hash].css'),
     new webpack.DefinePlugin({
       'process.env': {
         'NODE_ENV': JSON.stringify('production')
@@ -19,13 +24,29 @@ module.exports = {
       }
     }),
     new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.DedupePlugin()
+    new webpack.optimize.DedupePlugin(),
+    new HtmlWebpackPlugin({
+      template: 'index.template.ejs',
+      inject: 'body',
+    })
   ],
   module: {
     loaders: [{
       test: /\.js$/,
       loaders: ['babel'],
       include: path.join(__dirname, 'src')
+    }, {
+      test: /\.css$/,
+      loader: ExtractTextPlugin.extract(
+        'style-loader',
+        combineLoaders([{
+          loader: 'css-loader',
+          query: {
+            modules: true,
+            localIdentName: '[name]__[hash:base64:7]'
+          }
+        }])
+      )
     }]
   }
 };
